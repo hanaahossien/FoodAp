b996a8d8c53b47682227e1204d8afb7ec3456efc
hanaahossien <hanaa.hoosien@gmail.com>
2024-11-17 13:01:26 +0200

    add and update categories

--- src/Modules/categories/components/CategoriesList/CategoriesList.jsx
+++ src/Modules/categories/components/CategoriesList/CategoriesList.jsx
@@ -5,8 +5,63 @@ import NoData from "../../../shared/components/NoData/NoData";
 import axios from "axios";
 import DeleteConfirmation from "../../../shared/components/DeleteConfirmation/DeleteConfirmation";
 import { toast } from "react-toastify";
+import Modal from "react-bootstrap/Modal";
+import { useForm } from "react-hook-form";
 
 export default function CategoriesList() {
+  const [searchString, setSearchString] = useState("");
+
+  const {
+    register,
+    setValue,
+    handleSubmit,
+
+    formState: { errors },
+  } = useForm();
+
+  // modal update
+  const [idUpdate, setidUpdate] = useState(0);
+  const [showUpdate, setShowUpdate] = useState(false);
+  const handleCloseUpdate = () => setShowUpdate(false);
+  const handleShowUpdate = ({ prevname, previd }) => {
+    setidUpdate(previd);
+    setValue("name", prevname);
+    setShowUpdate(true);
+  };
+
+  let onSubmitupdate = async (data) => {
+    console.log(idUpdate);
+    try {
+      let nameCat = await axios.put(
+        `https://upskilling-egypt.com:3006/api/v1/Category/${idUpdate}`,
+        data,
+        { headers: { Authorization: localStorage.getItem("token") } }
+      );
+      handleCloseUpdate();
+      getCategoriesList();
+toast.warning("updated sucssed")    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // let selectCatById = async () => {
+  //   try {
+  //     let nameCat = await axios.get(
+  //       `https://upskilling-egypt.com:3006/api/v1/Category/${idUpdate}`,
+  //       { headers: { Authorization: localStorage.getItem("token") } }
+  //     );
+  //     console.log(nameCat.data.name);
+  //     // setupdatedvalue(nameCat.data.name);
+  //     console.log(nameCat.data.name, idUpdate);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+  // modal add
+  const [showAdd, setShowAdd] = useState(false);
+  const handleCloseAdd = () => setShowAdd(false);
+  const handleShowAdd = () => setShowAdd(true);
+
   const [id, setId] = useState(0);
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
@@ -38,7 +93,7 @@ export default function CategoriesList() {
       getCategoriesList();
 
       handleClose();
-      toast.success("this category deleted ");
+      toast.error("this category deleted ");
     } catch (error) {
       console.log(error);
     }
@@ -47,9 +102,121 @@ export default function CategoriesList() {
   useEffect(() => {
     getCategoriesList();
   }, []);
+  // const [newCategory, setnewCategory] = useState("")
+  let onSubmit = async (data) => {
+    try {
+      await axios.post(
+        "https://upskilling-egypt.com:3006/api/v1/Category/",
+        data,
+        {
+          headers: {
+            Authorization: localStorage.getItem("token"),
+          },
+        }
+      );
+      toast.success(`${data.name} added sucssed`);
+      handleCloseAdd();
+      getCategoriesList();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <>
+      <Modal
+        onSubmit={handleSubmit(onSubmitupdate)}
+        className=""
+        show={showUpdate}
+        onHide={handleCloseUpdate}
+      >
+        <Modal.Header className="border-0">
+          <Modal.Title className="w-100 p-4">
+            <div className="w-100 d-flex justify-content-between">
+              <div>update Category</div>
+              <div>
+                <i
+                  className="fa-solid fa-xmark text-danger  border border-2 border-danger px-2 p-1 rounded-circle "
+                  onClick={handleCloseUpdate}
+                ></i>
+              </div>
+            </div>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form className=" px-4">
+            <div>
+              <div className="input-group  my-4">
+                <input
+                  type="text"
+                  className="form-control border-0 bg-gray rounded-0 p-3"
+                  id="oldCat"
+                  placeholder="Category Name "
+                  {...register("name", { required: "Add Category Name" })}
+                />
+              </div>
+              <div>
+                {errors.name && (
+                  <div className="text-danger fs-6">{errors.name.message}</div>
+                )}
+              </div>
+            </div>
+            <div className="text-end">
+              <button className="btn btn-success mt-5 px-5 my-3">update</button>
+            </div>
+          </form>
+        </Modal.Body>
+      </Modal>
+
+      <Modal
+        onSubmit={handleSubmit(onSubmit)}
+        className=""
+        show={showAdd}
+        onHide={handleCloseAdd}
+      >
+        <Modal.Header className="border-0">
+          <Modal.Title className="w-100 p-4">
+            <div className="w-100 d-flex justify-content-between">
+              <div>Add Category</div>
+              <div>
+                <i
+                  className="fa-solid fa-xmark text-danger  border border-2 border-danger px-2 p-1 rounded-circle "
+                  onClick={handleCloseAdd}
+                ></i>
+              </div>
+            </div>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form className=" px-4">
+            <div>
+              <div className="input-group  my-4">
+                <input
+                  type="text"
+                  className="form-control border-0 bg-gray rounded-0 p-3"
+                  placeholder="Category Name "
+                  {...register("name", { required: "Add Category Name" })}
+                />
+              </div>
+              <div>
+                {errors.name && (
+                  <div className="text-danger fs-6">{errors.name.message}</div>
+                )}
+              </div>
+            </div>
+            <div className="text-end">
+              <button className="btn btn-success mt-5 px-5 my-3">save</button>
+            </div>
+          </form>
+        </Modal.Body>
+        {/* <Modal.Footer className="border-0">
+          <button className="btn btn-success  w-100 my-3">Login</button>
+           <Button className="btn  px-5 btn-lg btn-success"   onClick={handleCloseAdd}>
+            Save
+          </Button>
+        </Modal.Footer> */}
+      </Modal>
+
       <DeleteConfirmation
         show={show}
         handleClose={handleClose}
@@ -64,19 +231,23 @@ export default function CategoriesList() {
         }
       />
 
-      {CategoriesList.length > 0 ?  (
+      <div className="d-flex justify-content-between align-items-center my-3">
         <div>
-          <div className="d-flex justify-content-between align-items-center my-3">
-            <div>
-              <h3>Recipe Table Details</h3>
-              <span>You can check all details</span>
-            </div>
-            <div>
-              <button className="btn  px-5 btn-lg btn-success">
-                Add New Item{" "}
-              </button>
-            </div>
-          </div>
+          <h3>Recipe Table Details</h3>
+          <span>You can check all details</span>
+        </div>
+        <div>
+          <button
+            className="btn  px-5 btn-lg btn-success"
+            onClick={handleShowAdd}
+          >
+            Add New Item
+          </button>
+        </div>
+      </div>
+
+      {CategoriesList.length > 0 ? (
+        <div>
           <div>
             <table className="table table-striped">
               <thead>
@@ -89,7 +260,7 @@ export default function CategoriesList() {
                   </th>
 
                   <th className="py-4" scope="col">
-                  Date
+                    Date
                   </th>
 
                   <th className="py-4 rounded-end-4" scope="col">
@@ -113,7 +284,15 @@ export default function CategoriesList() {
                           handleShow(category.id);
                         }}
                       ></i>
-                      <i className="fa-regular fa-pen-to-square text-warning mx-1"></i>
+                      <i
+                        className="fa-regular fa-pen-to-square text-warning mx-1"
+                        onClick={() => {
+                          handleShowUpdate({
+                            prevname: category.name,
+                            previd: category.id,
+                          });
+                        }}
+                      ></i>
                     </td>
                   </tr>
                 ))}
@@ -121,9 +300,9 @@ export default function CategoriesList() {
             </table>
           </div>
         </div>
-      ):(
+      ) : (
         <NoData />
-      ) }
+      )}
     </>
   );
 }
--- src/Modules/recipe/components/RecipeList/RecipeList.jsx
+++ src/Modules/recipe/components/RecipeList/RecipeList.jsx
@@ -40,6 +40,7 @@ export default function RecipeList() {
   });
   return (
     <>
+
       <Header
         title={"Recipes"}
         item={"Items"}
@@ -47,97 +48,96 @@ export default function RecipeList() {
           "You can now add your items that any user can order it from the Application and you can edit"
         }
       />
+      <div className="d-flex justify-content-between align-items-center my-3">
+        <div>
+          <h3>Recipe Table Details</h3>
+          <span>You can check all details</span>
+        </div>
+        <div>
+          <button
+            className="btn  px-5 btn-lg btn-success"
+          >
+            Add New Item
+          </button>
+        </div>
+      </div>
       <div>
+        {RecipesList.length > 0 ? (
+          <>
+            <DeleteConfirmation
+              show={show}
+              handleClose={handleClose}
+              deltedItem={"recipeeeeeee"}
+              deleteFun={deleteRecipesList}
+            />
 
-      {RecipesList.length  > 0 ? (
-        <>
-          <DeleteConfirmation
-            show={show}
-            handleClose={handleClose}
-            deltedItem={"recipeeeeeee"}
-            deleteFun={deleteRecipesList}
-          />
-
-          <div className="d-flex justify-content-between align-items-center my-3">
             <div>
-              <h3>Recipe Table Details</h3>
-              <span>You can check all details</span>
-            </div>
-            <div>
-              <button className="btn  px-5 btn-lg btn-success">
-                Add New Item{" "}
-              </button>
-            </div>
-          </div>
+              <table className="table table-striped">
+                <thead>
+                  <tr className="table-active">
+                    <th className="p-4  rounded-start-4" scope="col">
+                      ID
+                    </th>
+                    <th className="py-4" scope="col">
+                      NAME
+                    </th>
 
-          <div>
-            <table className="table table-striped">
-              <thead>
-                <tr className="table-active">
-                  <th className="p-4  rounded-start-4" scope="col">
-                    ID
-                  </th>
-                  <th className="py-4" scope="col">
-                    NAME
-                  </th>
+                    <th className="py-4" scope="col">
+                      Image
+                    </th>
 
-                  <th className="py-4" scope="col">
-                    Image
-                  </th>
+                    <th className="py-4" scope="col">
+                      description
+                    </th>
+                    <th className="py-4" scope="col">
+                      Price
+                    </th>
+                    <th className="py-4" scope="col">
+                      Date
+                    </th>
 
-                  <th className="py-4" scope="col">
-                    description
-                  </th>
-                  <th className="py-4" scope="col">
-                    Price
-                  </th>
-                  <th className="py-4" scope="col">
-                    Date
-                  </th>
-
-                  <th className="py-4 rounded-end-4" scope="col">
-                    Action
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {RecipesList.map((recipe) => (
-                  <tr key={recipe.id}>
-                    <td className="p-4 " scope="row">
-                      {recipe.id}
-                    </td>
-                    <td className="p-4 " scope="row">
-                      {recipe.name}
-                    </td>
-                    <td className="py-4">
-                      <img
-                        src={`https://upskilling-egypt.com:3006/${recipe.imagePath}`}
-                        style={{ width: 60 }}
-                        className="rounded-2"
-                        alt=""
-                      />
-                    </td>
-                    <td className="py-4">{recipe.description}</td>
-                    <td className="py-4">{recipe.price}</td>
-                    <td className="py-4">{recipe.creationDate}</td>
-                    <td className="py-4">
-                      <i
-                        className="fa-regular fa-trash-can text-danger mx-2 "
-                        onClick={() => handleShow(recipe.id)}
-                      ></i>
-                      <i className="fa-regular fa-pen-to-square text-warning mx-2"></i>
-                    </td>
+                    <th className="py-4 rounded-end-4" scope="col">
+                      Action
+                    </th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        </>
-      ) : (
-        <NoData />
-      )}
+                </thead>
+                <tbody>
+                  {RecipesList.map((recipe) => (
+                    <tr key={recipe.id}>
+                      <td className="p-4 " scope="row">
+                        {recipe.id}
+                      </td>
+                      <td className="p-4 " scope="row">
+                        {recipe.name}
+                      </td>
+                      <td className="py-4">
+                        <img
+                          src={`https://upskilling-egypt.com:3006/${recipe.imagePath}`}
+                          style={{ width: 60 }}
+                          className="rounded-2"
+                          alt=""
+                        />
+                      </td>
+                      <td className="py-4">{recipe.description}</td>
+                      <td className="py-4">{recipe.price}</td>
+                      <td className="py-4">{recipe.creationDate}</td>
+                      <td className="py-4">
+                        <i
+                          className="fa-regular fa-trash-can text-danger mx-2 "
+                          onClick={() => handleShow(recipe.id)}
+                        ></i>
+                        <i className="fa-regular fa-pen-to-square text-warning mx-2"></i>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
             </div>
-
+          </>
+        ) : (
+          <NoData />
+        )}
+      </div>
     </>
   );
 }
